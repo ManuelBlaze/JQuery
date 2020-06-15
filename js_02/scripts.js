e1654449938ae8ac176e4e34b123fc13497bc67b
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-15 15:19:41 -0500

    Initial commit

---
+++ js_02/scripts.js
@@ -0,0 +1,28 @@
+// Selectores
+$(function () {
+  'use strict';
+
+  $('header');
+
+  $('header .contenedor');
+
+  $('.navegacion ').find('a');
+
+  $('#navegacion');
+
+  $('main article');
+
+  $('main article:first')
+
+  $('main article:first p ')
+
+  $('main article:first p:first ')
+
+  $('main article:first p:last ')
+
+  $('main article:first p:last ').hide();
+
+
+  $('nav ul li:first');
+
+});
---
+++ script.js
@@ -0,0 +1,19 @@
+$(function () {
+    'use strict';
+
+    $('.nuestros-servicios div:first').show();
+    $('.servicios nav a:first').addClass('activo');
+
+    $('.servicios nav a').on('click', mostrarTabs);
+
+    function mostrarTabs() {
+        $('.servicios nav a').removeClass('activo');
+        $(this).addClass('activo');
+        let enlace = $(this).attr('href');
+        $('.nuestros-servicios div').fadeOut();
+
+        $(enlace).fadeIn();
+        return false;
+    }
+
+});
